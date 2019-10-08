6148981d500263b99b87d2bce6a4e7a42b24d2fb
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-07 17:07:37 -0700

    Created a services directory and moved the function get_latest_packages() from app.py to it and imported it as package_service.py module. Created a home_views and package_views modules in views directory. Moved all the current views from app.y to home_views.py, and created a blueprint for home.

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -2,29 +2,10 @@ import flask
 
 from infrastructure.view_modifiers import response
 
+import pypi_org.services.package_service as package_service
+
 app = flask.Flask(__name__)
 
-
-def get_latest_packages():
-    return [
-        {'name': 'flask', 'version': '1.2.3'},
-        {'name': 'sqlalchemy', 'version': '2.2.0'},
-        {'name': 'passlib', 'version': '3.0.0'},
-    ]
-
-
-@app.route('/')
-@response(template_file='home/index.html')
-def index():
-    test_packages = get_latest_packages()
-    return {'packages': test_packages}
-    # return flask.render_template('home/index.html', packages=test_packages)
-
-
-@app.route('/about')
-@response(template_file='home/about.html')
-def about():
-    return {}
 
 
 if __name__ == '__main__':
