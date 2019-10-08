0e943db7afc65cdd96b3399a360341e6e18e4230
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-07 17:17:15 -0700

    Created a blueprint for project/package_name route. Created a view method called index with package_name: str as a parameter.

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -7,6 +7,5 @@ import pypi_org.services.package_service as package_service
 app = flask.Flask(__name__)
 
 
-
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ pypi_org/views/package_views.py
@@ -0,0 +1,11 @@
+import flask
+
+from pypi_org.infrastructure.view_modifiers import response
+
+blueprint = flask.Blueprint('package', __name__, template_folder='templates')
+
+
+@blueprint.route('project/<package_name>')
+@response(template_file='packages/details.html')
+def index(package_name: str):
+    return f'Package details for {package_name}'
