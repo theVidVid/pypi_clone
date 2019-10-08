40bb19ba85fbfdf4b13bfad227e4864421cabcb3
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-07 17:43:00 -0700

    Fixed a few typos in package_views.py, swapped order of register_blueprints() and main() functions in app.py.

--- pypi_org/views/package_views.py
+++ pypi_org/views/package_views.py
@@ -2,10 +2,12 @@ import flask
 
 from pypi_org.infrastructure.view_modifiers import response
 
-blueprint = flask.Blueprint('package', __name__, template_folder='templates')
+import pypi_org.services.package_service as package_service
+
+blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 
-@blueprint.route('project/<package_name>')
-@response(template_file='packages/details.html')
+@blueprint.route('/project/<package_name>')
+# @response(template_file='packages/details.html')
 def package_details(package_name: str):
     return f'Package details for {package_name}'
