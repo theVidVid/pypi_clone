47e7161fb199f23e996a38a1c337d3940d2d59b7
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-10 11:19:11 -0700

    Created base class for packages in a module named packages.py in Data directory.

---
+++ pypi_org/data/package.py
@@ -0,0 +1,28 @@
+import sqlalchemy as sa
+
+
+class Package:
+    """Class representing Packages from Pypi.org"""
+
+    # Database table name will be packages instead of Package
+    __tablename__ = 'packages'
+
+    id = sa.Column(sa.String, primary_key=True)
+    created_date = sa.Column(sa.DATETIME)
+    summary = sa.Column(sa.String)
+    description = sa.Column(sa.String)
+
+    home_page = sa.Column(sa.String)
+    docs_url = sa.Column(sa.String)
+    package_url = sa.Column(sa.String)
+
+    author_name = sa.Column(sa.String)
+    author_email = sa.Column(sa.String)
+
+    license = sa.Column(sa.String)
+
+    # maintainers
+    # releases
+
+    def __repr__(self):
+        return f'<Package {self.id}>'
