a2f17c6ed608edd363120f3c7860d344e99a38ce
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-18 18:57:34 -0700

    Added import statements for all the newly created classes to the __all_models helper module.

--- pypi_org/data/__all_models.py
+++ pypi_org/data/__all_models.py
@@ -1,4 +1,18 @@
-# noinspection PyUnResolvedReferences
-from pypi_org.data.package import Package
+# Add all your SQLAlchemy models here.
+# This allows a simple import of this file when
+# it is needed to preload the models and ensure that they are all loaded.
 
-# And all future classes to be added
+# noinspection PyUnResolvedReferences
+import pypi_org.data.downloads
+# noinspection PyUnResolvedReferences
+import pypi_org.data.languages
+# noinspection PyUnResolvedReferences
+import pypi_org.data.licenses
+# noinspection PyUnResolvedReferences
+import pypi_org.data.maintainers
+# noinspection PyUnResolvedReferences
+import pypi_org.data.package
+# noinspection PyUnResolvedReferences
+import pypi_org.data.releases
+# noinspection PyUnResolvedReferences
+import pypi_org.data.users
