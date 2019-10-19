d5deb3b6b89e37818b3ae5ca599a600a48bf8e00
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-18 18:23:52 -0700

    Created license class that represents a particular software license.

---
+++ pypi_org/data/licenses.py
@@ -0,0 +1,13 @@
+import datetime
+import sqlalchemy
+from pypi_org.data.modelbase import SqlAlchemyBase
+
+
+class License(SqlAlchemyBase):
+    """A class representing a particular software license."""
+    __tablename__ = 'licenses'
+
+    id = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now, index=True)
+    description = sqlalchemy.Column(sqlalchemy.String)
