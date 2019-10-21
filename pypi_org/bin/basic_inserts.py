1a31b9ccd7672882a760ddf0a806bca3dcbdf683
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-21 11:03:02 -0700

    Created a directory named bin that will contain maintanence and admin related scripts. Created a module in that very directory that will initialize the database.

---
+++ pypi_org/bin/basic_inserts.py
@@ -0,0 +1,17 @@
+import os
+import pypi_org.data.db_session as db_session
+
+
+def main():
+    init_db()
+
+
+def init_db():
+    top_folder = os.path.dirname(__file__)
+    rel_file = os.path.join('..', 'db', 'pypi.sqlite')
+    db_file = os.path.abspath(os.path.join(top_folder, rel_file))
+    db_session.global_init(db_file)
+
+
+if __name__ == '__main__':
+    main()
