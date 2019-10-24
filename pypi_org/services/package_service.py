0bb8f5464a0396bf2c1151348948c9f54d5e71ee
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-24 14:11:56 -0700

    Reformatted function calls in package_service module.

--- pypi_org/services/package_service.py
+++ pypi_org/services/package_service.py
@@ -8,11 +8,11 @@ from pypi_org.data.releases import Release
 def get_latest_releases(limit=10) -> List[Release]:
     session = db_session.create_session()
 
-    releases = session.query(Release).\
-        options(sqlalchemy.orm.joinedload(Release.package)).\
-        order_by(Release.created_date.desc()).\
-        limit(limit).\
-        all()
+    releases = session.query(Release)\
+        .options(sqlalchemy.orm.joinedload(Release.package))\
+        .order_by(Release.created_date.desc())\
+        .limit(limit)\
+        .all()
 
     session.close()
 
@@ -35,9 +35,9 @@ def get_package_by_id(package_id: str) -> Optional[Package]:
 
     package_id = package_id.strip().lower()
     session = db_session.create_session()
-    package = session.query(Package).\
-        filter(Package.id == package_id).\
-        first()
+    package = session.query(Package)\
+        .filter(Package.id == package_id)\
+        .first()
     session.close()
 
     return package
