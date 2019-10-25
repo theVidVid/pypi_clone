a780373d35011220cac668ba6a931b688b964935
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-25 12:19:43 -0700

    Added connect_args to create_engine() method. to prevent threading errors.

--- pypi_org/data/db_session.py
+++ pypi_org/data/db_session.py
@@ -19,7 +19,8 @@ def global_init(db_file: str):
     conn_str = 'sqlite:///' + db_file.strip()
     print(f"Connecting to DB with {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False)
+    engine = sa.create_engine(conn_str, echo=False,
+                              connect_args={'check_same_thread': False})
     __factory = orm.sessionmaker(bind=engine)
 
     # noinspection PyUnResolvedReferences
