ee787ee560aa684678725895098e2d236bdec70b
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-10-30 13:43:55 -0700

    Added audit class to version control.

---
+++ pypi_org/data/audit.py
@@ -0,0 +1,14 @@
+import datetime
+import sqlalchemy as sa
+from pypi_org.data.modelbase import SqlAlchemyBase
+
+
+class Audit(SqlAlchemyBase):
+    """A class representing a particular software license."""
+    __tablename__ = 'auditing'
+    __table_args__ = {'extend_existing': True}
+
+    id = sa.Column(sa.String, primary_key=True)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now,
+                             index=True)
+    description = sa.Column(sa.String)
