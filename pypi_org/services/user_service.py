6edb6d52d5a8c3c3142cae3ffe69d73ae7b87014
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-11-18 11:49:26 -0800

    Created function login_user() that takes in email and password as str args and creates db_session that matches registered user's email, and calls verify_hash method that verifies a registered user's hashed_password and returns the user if both pass.

--- pypi_org/services/user_service.py
+++ pypi_org/services/user_service.py
@@ -23,18 +23,32 @@ def create_user(name: str, email: str, password: str) -> Optional[User]:
     user = User()
     user.email = email
     user.name = name
-    user.password = hash_text(password)
+    user.hashed_password = hash_text(password)
 
     session = db_session.create_session()
     session.add(user)
     session.commit()
+
     return user
 
 
-def hash_text(text: str) -> bool:
+def hash_text(text: str) -> str:
     hashed_text = crypto.encrypt(text, rounds=171204)
     return hashed_text
 
 
 def verify_hash(hashed_text: str, plain_text: str) -> bool:
     return crypto.verify(plain_text, hashed_text)
+
+
+def login_user(email: str, password: str) -> Optional[User]:
+    session = db_session.create_session()
+
+    user = session.query(User).filter(User.email == email).first()
+    if not user:
+        return None
+
+    if not verify_hash(user.hashed_password, password):
+        return None
+
+    return user
