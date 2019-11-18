faf630bcb2433012ca4bd04afc023e5f572a7af6
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-11-18 15:09:18 -0800

    Added user_id entry to every view method in home_views module.

--- pypi_org/views/home_views.py
+++ pypi_org/views/home_views.py
@@ -1,5 +1,6 @@
 import flask
 
+from infrastructure import cookie_auth
 from pypi_org.infrastructure.view_modifiers import response
 import pypi_org.services.package_service as package_service
 import pypi_org.services.user_service as user_service
@@ -15,6 +16,7 @@ def index():
         'package_count': package_service.get_package_count(),
         'release_count': package_service.get_release_count(),
         'user_count': user_service.get_user_count(),
+        'user_id': cookie_auth.get_user_id_via_auth_cookie(flask.request),
     }
     # return flask.render_template('home/index.html', packages=test_packages)
 
@@ -22,4 +24,6 @@ def index():
 @blueprint.route('/about')
 @response(template_file='home/about.html')
 def about():
-    return {}
+    return {
+        'user_id': cookie_auth.get_user_id_via_auth_cookie(flask.request),
+    }
