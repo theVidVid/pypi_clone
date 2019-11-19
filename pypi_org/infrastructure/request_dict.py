03f794f704e7106da6fbca55ee1f51a4673206cf
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-11-19 15:22:33 -0800

    Created a module named request_dict in infrastructure directory that centralizes the data from pypi into one source.

---
+++ pypi_org/infrastructure/request_dict.py
@@ -0,0 +1,18 @@
+from flask import Request
+
+
+class RequestDictionary(dict):
+    def __getattr__(self, key):
+        return self.get(key)
+
+
+def create(request: Request, **route_args) -> RequestDictionary:
+    data = {
+        **request.args,  # The key/value pairs in the URL query string
+        **request.headers,  # Header values
+        **request.form,  # The key/value pairs in body, from a HTML post form
+        **route_args,  # Add additional args that the method accesses,
+        # if they want them merged
+    }
+
+    return RequestDictionary(data)
