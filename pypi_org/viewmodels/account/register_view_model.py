0cb73add0493af78a88c1a846450782936d37422
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-11-25 18:26:23 -0800

    Added an additional error handler to validate() class method that calls user_service.find_user_by_email() to verify whether the email address already exists in the database.

--- pypi_org/viewmodels/account/register_view_model.py
+++ pypi_org/viewmodels/account/register_view_model.py
@@ -12,9 +12,11 @@ class RegisterViewModel(ViewModelBase):
     def validate(self):
         if not self.name or not self.name.strip():
             self.error = 'You must specify a name.'
-        elif not self.email or not self.email.strip():
+        elif not self.email or not self.email.lower().strip():
             self.error = 'You must specify an email address.'
-        elif not self.password:
+        elif not self.password or not self.password.strip():
             self.error = 'You must specify a password.'
-        elif len(self.password.strip() < 5):
+        elif len(self.password.strip()) < 5:
             self.error = 'The password must be at least 5 characters.'
+        elif user_service.find_user_by_email(self.email):
+            self.error = 'A user with that email already exists.'
