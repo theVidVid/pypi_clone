c897a8939592708b29e49d2a447af633a66b9a1f
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-11-25 18:33:24 -0800

    Updated register_view_models module.

--- pypi_org/viewmodels/account/register_view_model.py
+++ pypi_org/viewmodels/account/register_view_model.py
@@ -12,9 +12,9 @@ class RegisterViewModel(ViewModelBase):
     def validate(self):
         if not self.name or not self.name.strip():
             self.error = 'You must specify a name.'
-        elif not self.email or not self.email.lower().strip():
+        elif not self.email or not self.email.strip():
             self.error = 'You must specify an email address.'
-        elif not self.password or not self.password.strip():
+        elif not self.password:
             self.error = 'You must specify a password.'
         elif len(self.password.strip()) < 5:
             self.error = 'The password must be at least 5 characters.'
