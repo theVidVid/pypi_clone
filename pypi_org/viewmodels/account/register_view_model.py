c8ed8d4cd61e55b6a938af351835d73c6f7e32ec
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-11-25 16:55:25 -0800

    Created a register_view_model module containing RegisterViewModel() class that inherits from ViewModelBase class and passes name, email, and password as instance attributes with each one calling the request_dict call to creating its own specific values.

---
+++ pypi_org/viewmodels/account/register_view_model.py
@@ -0,0 +1,10 @@
+from services import user_service
+from viewmodels.shared.viewmodelbase import ViewModelBase
+
+
+class RegisterViewModel(ViewModelBase):
+    def __init__(self):
+        super().__init__()
+        self.name = self.request_dict.name
+        self.email = self.request_dict.email.lower().strip()
+        self.password = self.request_dict.password.strip()
